5cbe70783cb04e19ba8de5915aa8efcb50cfd4d3
samandar12 <samandarkomiljonov1212@gmail.com>
2023-05-17 21:41:24 +0500

    Bu js1-dars kodlari yuklandi

---
+++ JS/main.js
@@ -0,0 +1,32 @@
+// console.log("4*4");
+// alert("saytimizga hush kelibsiz!");
+// document.write("Men Body oynasida korinaman!");
+// let x=2;
+// let y=5;
+// x=10;
+// let s=2,r=5,g=7
+// document.write(s+r+g);
+
+/* JS data types*/
+// let x = 5;
+// let y = "JavaScript"
+// document.write(typeof y);
+
+/* js arifmetik amallar*/
+let x =50
+// x+=1
+// x-=2
+// x*=55
+// x/=2
+// let qoldiq =x%12
+// let natija =x**2 /* *50*50*50*50*50 => */
+/* ++x; */
+/* --x; */
+/* x--; */
+/* x++; */
+/*let son="5"*/
+/*let matnson="55"*/
+/*document.write(matnson/5)*/
+
+
+
